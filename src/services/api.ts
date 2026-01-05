cc858a3b21c32562a5dc9ad824d9b9caf76f7fce
Spectro451 <bastyfarias30@gmail.com>
2026-01-05 02:58:55 -0300

    ahora si funciona completamente el front en coolify

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,30 +2,30 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://kiwipetsbackend.onrender.com",
+  baseURL: "https://kiwibd.kiwi-dev.xyz",
   timeout: 10000,
 });
 
 //envia el token al header de cada consulta
 api.interceptors.request.use(
-  async config => {
-    const token = await AsyncStorage.getItem('token');
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
     if (token && config.headers) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
     return config;
   },
-  error => Promise.reject(error)
+  (error) => Promise.reject(error)
 );
 
 //interceptor loco para cuando no tenga acceso
 api.interceptors.response.use(
-  response => response,
-  async error => {
+  (response) => response,
+  async (error) => {
     if (error.response?.status === 401) {
       console.log("Token inválido o expirado");
-      await AsyncStorage.removeItem('token');
-      await AsyncStorage.removeItem('user');
+      await AsyncStorage.removeItem("token");
+      await AsyncStorage.removeItem("user");
     }
     return Promise.reject(error);
   }
